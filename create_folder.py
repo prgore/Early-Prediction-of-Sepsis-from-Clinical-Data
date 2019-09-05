747e2b3f6e80c82ffe3fc5e1ede6cf3b85fa6899
company <quanglvhust@gmail.com>
2019-09-05 16:35:54 +0700

    delete all old files

--- create_folder.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def create_folder(dic_name):
-    if not os.path.exists(dic_name):
-       os.makedirs(dic_name)
--- write_log.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-def write_log(file_name, list_of_dict):
-    key = list_of_dict[0].keys()
-    with open(file_name,'w') as output_file:
-        dict_writer = csv.DictWriter(output_file,key)
-        dict_writer.writeheader()
-        dict_writer.writerows(list_of_dict)
